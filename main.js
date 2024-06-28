f1108ac15d07833e84439100cb3b25a60a267402
Tray Simpson <tdsimpson123@gmail.com>
2024-06-27 21:18:17 -0600

    Adds basic key reading and rendering

--- main.js
+++ main.js
@@ -3,15 +3,100 @@
 const MAX_GUESSES = 6;
 const WORD_LENGTH = 5;
 
-let grid = document.querySelector('#guessing-grid');
+let guesses = [ [ ] ];
 
-for (let i = 0; i < MAX_GUESSES; i++) {
-    let row = document.createElement('div');
-    row.classList.add('guess-row');
-    for (let j = 0; j < WORD_LENGTH; j++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
+function init() {
+    let grid = document.querySelector('#guessing-grid');
+    document.addEventListener("keydown", handleKeyPress);
+
+    for (let i = 0; i < MAX_GUESSES; i++) {
+        let row = document.createElement('div');
+        row.classList.add('guess-row');
+        for (let j = 0; j < WORD_LENGTH; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
+}
+
+function handleKeyPress(e) {
+    try {
+        let letter = e.key;
+        checkLetter(letter);
+        letter = letter.toUpperCase();
+        addLetterToGuesses(letter);
+        checkForWord();
+        render();
+    } catch (e) {
+        if (e instanceof InvalidLetterException) {
+            displayError('Invalid letter!');
+        } else {
+            console.error(Error);
+        }
+    }
+}
+
+function checkLetter(letter) {
+    if (letter.length !== 1) {
+        throw new Error('Invalid letter length');
+    }
+    let regex = /^[a-zA-Z]+$/;
+    if (!regex.test(letter)) {
+        throw new InvalidLetterException();
+    }
+}
+
+function addLetterToGuesses(letter) {
+    let latestWord = guesses[guesses.length - 1];
+    if (latestWord.length === WORD_LENGTH) {
+        if (guesses.length === MAX_GUESSES) {
+            throw new Error('No more guesses left');
+        }
+        guesses.push([letter]);
+    } else {
+        guesses[guesses.length - 1] = [...latestWord, letter];
+    }
+}
+
+function checkForWord() {
+    let latestWord = guesses[guesses.length - 1];
+    if (latestWord.length === WORD_LENGTH) {
+        let word = latestWord.join('');
+        //TODO
+    }
+}
+
+function render() {
+    let grid = document.querySelector('#guessing-grid');
+
+    
+    for(let i = 0; i < guesses.length; i++) {
+        let row = grid.children[i];
+        let word = guesses[i];
+        for(let j = 0; j < word.length; j++) {
+            let cell = row.children[j];
+            cell.textContent = word[j];
+        }
+    }
+}
+
+function displayError(message) {
+    let error = document.querySelector('#error');
+    error.textContent = message;
+    error.style.display = 'block';
+    setTimeout(() => {
+        error.style.display = 'none';
+    }, 3000);
+}
+
+
+
+init();
+
+class InvalidLetterException extends Error {
+    constructor() {
+        super('Invalid letter');
+    }
 }
